69c6ac63df15edb4657cabcb68afebb5d7822e28
astrobemo <astrobemo@gmail.com>
2024-12-01 17:35:06 +0700

    add intersection test

--- tests/intersection.test.ts
+++ tests/intersection.test.ts
@@ -18,4 +18,24 @@ describe('Intersection data type', () => {
         expect(c.a).toBe(1);
         expect(c.b).toBe("TEST");
     });
+
+    it('should support intersection of interface', () => {
+        interface A {
+            a: number;
+        }
+
+        interface B {
+            b: string;
+        }
+
+        type C = A & B;
+
+        const c: C = {
+            a: 1,
+            b: "TEST"
+        };
+
+        expect(c.a).toBe(1);
+        expect(c.b).toBe("TEST");
+    }
 });
